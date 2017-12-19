c9714eb73112a9ebeca07b6dd81865fe3225d8ab
Ronak Lakhotia <ronak.lakhotia999@gmail.com>
2017-12-19 11:36:08 +0800

    Canvas manipulation

--- js/logic.js
+++ js/logic.js
@@ -26,5 +26,43 @@ window.onload = function() {
 		document.location.reload();
 	}
 
+	document.getElementById("tic").addEventListener("click", function(e) {
+		boxClick(e.target.id);
+	});
+
+	function boxClick(numId) {
+		box = document.getElementById(numId);
+		ctx = box.getContext("2d");
+
+		switch(numId) {
+			case "canvas1": num = 1;
+						  break;
+
+			case "canvas2": num = 2;
+						  break;
+
+			case "canvas3": num = 3;
+						  break;
+
+			case "canvas4": num = 4;
+						  break;
+
+			case "canvas5": num = 5;
+						  break;
+
+			case "canvas6": num = 6;
+						  break;
+
+			case "canvas7": num = 7;
+						  break;
+
+			case "canvas8": num = 8;
+						  break;
+						  						  
+			case "canvas9": num = 9;
+						  break;
+						  
+		}
+	}
 
 }

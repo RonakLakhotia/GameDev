9745ce4dd68ac99a7d53ab6e7e60a9059e9e5a74
Ronak Lakhotia <ronak.lakhotia999@gmail.com>
2017-12-19 11:15:08 +0800

    Start with logic

---
+++ js/logic.js
@@ -0,0 +1,26 @@
+//anonymous function
+window.onload = function {
+
+	//variable declaration
+
+	var num;
+	var box;
+	var ctx;
+	var turn = 1;
+	var filled;
+	var symbol;
+	var winner;
+	var isGameOver = false;
+
+	filled = [false, false, false, false, false, false, false, false, false];
+	symbol = ['', '', '', '', '', '', '', '', ''];
+
+	winner = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+
+	//new game button
+
+	var button = document.getElementById("new");
+	button.addEventListener("click", newGame);
+
+
+}
